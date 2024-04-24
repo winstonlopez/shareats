e0ca105383fbac3e762ccd4772ac4fe27294d0e2
winstonlopez <ev.winston2304@gmail.com>
2024-04-24 10:13:31 +0800

    fixed rotate

--- src/routes/recipes.ts
+++ src/routes/recipes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import Recipe from '../models/recipe';
